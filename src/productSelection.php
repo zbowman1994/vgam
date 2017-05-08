c6c8be193d9ccab3614e4067be8200293062053d
Nathan Mackenzie <ntmk@telus.net>
2017-05-08 16:30:36 -0700

    updating all source code we have so far

---
+++ src/productSelection.php
@@ -0,0 +1,12 @@
+<?php require 'view/header.html'; ?>
+
+<button type="button"><a href = "getAllProducts.php">all products</a></button>
+<button type="button"><a href = "getAllWiiGames.php">wii games</a></button>
+<button type="button"><a href = "getAllPlaystationGames.php">play station games</a></button>
+<button type="button"><a href = "getAllMiscItems.php">Misc Items</a></button>
+<button type="button"><a href = "index.php">Main</a></button>
+<button type="button"><a href = "">contact us</a></button>
+<button type="button"><a href = "">login</a></button>
+
+
+<?php require 'view/footer.php'; ?>
