5cd0ac9726fc7c1bc5cf0317a1c098e2c9697e8c
Nathan Mackenzie <ntmk@telus.net>
2017-05-08 16:27:38 -0700

    updating all source code we have so far

---
+++ src/cart.php
@@ -0,0 +1,37 @@
+<?php
+    
+    
+    require_once 'model/db_connect.php';
+    require_once 'model/db_functions.php';
+	
+	// Get products from db
+	
+	?>
+<button type="button">Empty Cart</button>
+<button type="button"><a href = "index.php">Continue shopping</a></button>
+<button type="button">Checkout</button>
+    <table class="table table-bordered table-striped" id="names">
+        <thead>
+            <tr>
+                <th scope="col">Product Name</th>
+				<th scope="col">Product Price</th>
+				<th scope="col">Quantity</th>
+				<th scope="col">Add/Remove product</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- use foreach loop to fetch contents of each row -->
+          
+		   <?php echo $_REQUEST['product_id']; ?>
+		    <!-- need to update for cart instead of listing all the products again -->
+			
+            <tr>
+                <td><?php echo $_POST['product_name']; ?></td>
+				<td>$<?php echo $_POST['product_price']; ?></td>
+							 <?php echo $_POST['product_id']; ?>
+				
+				<button type="submit">Remove</button></td>
+            </tr>
+            
+        </tbody>
+    </table>
