94622276bd05b0f8712c3f122708457b755471d5
Nathan Mackenzie <ntmk@telus.net>
2017-05-08 17:19:37 -0700

    database eer diagram main source code

---
+++ src/cart.php
@@ -0,0 +1,40 @@
+<?php
+    
+    
+    require_once 'model/db_connect.php';
+    require_once 'model/db_functions.php';
+	
+	// Get products from db
+	
+	?>
+<button type="button">Empty Cart</button>
+<button type="button"><a href = "index.php">Continue shopping</a></button>
+<button type="button">Checkout</button>
+    <table class="table table-bordered table-striped" id="names">
+        <thead>
+            <tr>
+				<th scope="col">Product ID</th>
+                <th scope="col">Product Name</th>
+				<th scope="col">Product Price</th>
+				<th scope="col">Quantity</th>
+				<th scope="col">Remove product</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- use foreach loop to fetch contents of each row -->
+          
+		  
+		    <!-- need to update for cart instead of listing all the products again -->
+			
+            <tr>
+                <td><?php echo $_POST['product_id']; ?></td>
+				<td><?php echo $_POST['product_name']; ?></td>
+				<td>$<?php echo $_POST['product_price']; ?></td>
+				
+				
+				
+				<td><button type="submit">Remove</button></td>
+            </tr>
+            
+        </tbody>
+    </table>
---
+++ src/name.php
@@ -0,0 +1,41 @@
+<?php
+namespace ics199;
+
+class Name {
+    private $firstName;
+    private $lastName;
+    
+    private static $nameCount = 0;
+    
+    public function __construct($firstName, $lastName) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        
+        self::$nameCount++;
+    }
+    
+    public static function getObjectCount() {
+        return self::$nameCount;
+    }
+    
+    public function getFirstName() {
+        return $this->firstName;
+    }
+    
+    public function setFirstName($firstName) {
+        $this->firstName = $firstName;
+    }
+    
+    public function getLastName() {
+        return $this->lastName;
+    }
+    
+    public function setLastName($lastName) {
+        $this->lastName = $lastName;
+    }
+    
+    public function __toString() {
+        return "$this->firstName $this->lastName";
+    }
+}
+?>
