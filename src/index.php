bc69a15e1f6b1444bda2e0760f29ec51ed452b85
jasmine <jasminekivari@gmail.com>
2017-05-09 15:44:57 -0700

    Replace index.php

--- src/index.php
+++ src/index.php
@@ -1,8 +1,13 @@
+<link rel="stylesheet" type ="text/css" href="css/style.css" > 
 <?php require 'view/header.html'; ?>
 
+<button type="button" id="main"><a href="index.php">Main</a></button>
 <button type="button" id="contactus"><a href = "index.php">Contact Us</a></button>
 <button type="button" id="products"><a href = "productSelection.php">Products</a></button>
-<button type="button"id="login><a href = "index.php">Login *to-do: create login</a></button>
+<button type="button" id="login"><a href = "index.php">Login</a></button>
+                                                    
+
+                                                                                      
 
 
 <?php require 'view/footer.php'; ?>
