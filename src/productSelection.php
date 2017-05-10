61a9c01732835aedd52ebc42e2d569b31de58d8e
jasmine <jasminekivari@gmail.com>
2017-05-10 15:17:10 -0700

    added-pictures-and-big-button

--- src/productSelection.php
+++ src/productSelection.php
@@ -1,7 +1,7 @@
 <?php require 'view/header.html'; ?>
 <link rel="stylesheet" type ="text/css" href="css/style.css" > 
 <button type="button" id="main"><a href = "index.php">Main</a></button>
-<button type="button" id="contactus"><a href = "">Contact Us</a></button>
+<button type="button" id="contactus"><a href = "contact.php">Contact Us</a></button>
 <button type="button" id="login"><a href = "">Login</a></button>
 <div>
     <table>
@@ -18,20 +18,18 @@
       <td>
       <div class="imgContainer">
 				<div>
-					<img src="css/images/ps4.jpg" width="500px" height="400px"/>
 				</div>
 				<div class="imgButton">
-					<button type="button" id="playstationgames"><a href = "getAllPlaystationGames.php">Playstation Games</a></button>
+					<button type="button" id="playstationgames"><a href = "getAllPlaystationGames.php"><img src="css/images/ps4.jpg" width="500px" height="400px"/>Playstation Games</a></button>
 				</div>
          </div>
 			</td>
         <td>
       <div class="imgContainer">
 				<div>
-					<img src="css/images/download.jpg" width="500px" height="400px"/>
 				</div>
 				<div class="imgButton">
-					<button type="button" id="miscitems"><a href = "getAllMiscItems.php">Misc Items</a></button>
+					<button type="button" id="miscitems"><a href = "getAllMiscItems.php"><img src="css/images/download.jpg" width="500px" height="400px"/>Misc Items</a></button>
 				</div>
          </div>
 			</td>
@@ -42,13 +40,7 @@
     
     
     
-    
-    
-    
-    
-    
-    
-    
+
 <button type="button" id="allproducts"><a href = "getAllProducts.php">All Products</a></button>
 
 
