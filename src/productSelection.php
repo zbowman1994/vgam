1d65d54660b651479035667068d759b684b09376
jasmine <jasminekivari@gmail.com>
2017-05-10 14:09:20 -0700

    added-pictures-and-big-button

--- src/productSelection.php
+++ src/productSelection.php
@@ -4,12 +4,53 @@
 <button type="button" id="contactus"><a href = "">Contact Us</a></button>
 <button type="button" id="login"><a href = "">Login</a></button>
 <div>
-
+    <table>
+		<tr>
+			<td>
+      <div class="imgContainer">
+				<div>
+				</div>
+				<div class="imgButton">
+					<button type="button" id="wii"><a href = "getAllWiiGames.php"><img src="css/images/images.jpg" width="500px" height="400px"/>Wii Games</a></button>
+				</div>
+         </div>
+			</td>
+      <td>
+      <div class="imgContainer">
+				<div>
+					<img src="css/images/ps4.jpg" width="500px" height="400px"/>
+				</div>
+				<div class="imgButton">
+					<button type="button" id="playstationgames"><a href = "getAllPlaystationGames.php">Playstation Games</a></button>
+				</div>
+         </div>
+			</td>
+        <td>
+      <div class="imgContainer">
+				<div>
+					<img src="css/images/download.jpg" width="500px" height="400px"/>
+				</div>
+				<div class="imgButton">
+					<button type="button" id="miscitems"><a href = "getAllMiscItems.php">Misc Items</a></button>
+				</div>
+         </div>
+			</td>
+            
+            </tr>
+</table>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 <button type="button" id="allproducts"><a href = "getAllProducts.php">All Products</a></button>
-<button type="button" id="wiigames"><a href = "getAllWiiGames.php">Wii Games</a></button>
-<button type="button" id="playstationgames"><a href = "getAllPlaystationGames.php">play station games</a></button>
-<button type="button" id="miscitems"><a href = "getAllMiscItems.php">Misc Items</a></button>
+
 
 
 </div>
