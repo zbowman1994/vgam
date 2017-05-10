7fbb230b10e7af6da465d260e5d48bca30e90a27
jasmine <jasminekivari@gmail.com>
2017-05-10 12:29:48 -0700

    updated-button-ids

--- src/productSelection.php
+++ src/productSelection.php
@@ -1,12 +1,13 @@
 <?php require 'view/header.html'; ?>
+<link rel="stylesheet" type ="text/css" href="css/style.css" > 
 
-<button type="button"><a href = "getAllProducts.php">all products</a></button>
-<button type="button"><a href = "getAllWiiGames.php">wii games</a></button>
-<button type="button"><a href = "getAllPlaystationGames.php">play station games</a></button>
-<button type="button"><a href = "getAllMiscItems.php">Misc Items</a></button>
-<button type="button"><a href = "index.php">Main</a></button>
-<button type="button"><a href = "">contact us</a></button>
-<button type="button"><a href = "">login</a></button>
+<button type="button" id="allproducts"><a href = "getAllProducts.php">All Products</a></button>
+<button type="button" id="wiigames"><a href = "getAllWiiGames.php">Wii Games</a></button>
+<button type="button" id="playstationgames"><a href = "getAllPlaystationGames.php">play station games</a></button>
+<button type="button" id="miscitems"><a href = "getAllMiscItems.php">Misc Items</a></button>
+<button type="button" id="main"><a href = "index.php">Main</a></button>
+<button type="button" id="contactus"><a href = "">contact us</a></button>
+<button type="button" id="login"><a href = "">login</a></button>
 
 
 <?php require 'view/footer.php'; ?>
