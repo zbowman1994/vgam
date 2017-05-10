df68181d60dbdfd9a37a83f545f65ad5896e44ab
jasmine <jasminekivari@gmail.com>
2017-05-10 13:14:34 -0700

    added-images

--- src/productSelection.php
+++ src/productSelection.php
@@ -1,13 +1,17 @@
 <?php require 'view/header.html'; ?>
 <link rel="stylesheet" type ="text/css" href="css/style.css" > 
+<button type="button" id="main"><a href = "index.php">Main</a></button>
+<button type="button" id="contactus"><a href = "">Contact Us</a></button>
+<button type="button" id="login"><a href = "">Login</a></button>
+<div>
 
+    
 <button type="button" id="allproducts"><a href = "getAllProducts.php">All Products</a></button>
 <button type="button" id="wiigames"><a href = "getAllWiiGames.php">Wii Games</a></button>
 <button type="button" id="playstationgames"><a href = "getAllPlaystationGames.php">play station games</a></button>
 <button type="button" id="miscitems"><a href = "getAllMiscItems.php">Misc Items</a></button>
-<button type="button" id="main"><a href = "index.php">Main</a></button>
-<button type="button" id="contactus"><a href = "">contact us</a></button>
-<button type="button" id="login"><a href = "">login</a></button>
 
+
+</div>
 
 <?php require 'view/footer.php'; ?>
