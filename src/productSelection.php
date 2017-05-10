ea5b5dde4aa35b8bb5d0194efd392df7b4bbe507
ICS User <ICS User>
2017-05-10 15:21:43 -0700

    added contact us and capitalized some stuf

--- src/productSelection.php
+++ src/productSelection.php
@@ -1,12 +1,12 @@
 <?php require 'view/header.html'; ?>
 
-<button type="button"><a href = "getAllProducts.php">all products</a></button>
-<button type="button"><a href = "getAllWiiGames.php">wii games</a></button>
-<button type="button"><a href = "getAllPlaystationGames.php">play station games</a></button>
+<button type="button"><a href = "getAllProducts.php">All products</a></button>
+<button type="button"><a href = "getAllWiiGames.php">Wii games</a></button>
+<button type="button"><a href = "getAllPlaystationGames.php">PlayStation games</a></button>
 <button type="button"><a href = "getAllMiscItems.php">Misc Items</a></button>
 <button type="button"><a href = "index.php">Main</a></button>
-<button type="button"><a href = "">contact us</a></button>
-<button type="button"><a href = "">login</a></button>
+<button type="button"><a href = "contact.php">Contact us</a></button>
+<button type="button"><a href = "">Login</a></button>
 
 
 <?php require 'view/footer.php'; ?>
