3d0eb2314a14e3b405add6fd143551fe01fdada5
Nathan Mackenzie <ntmk@telus.net>
2017-05-11 13:06:22 -0700

    updating still working on remove

---
+++ src/model/deleteFromCart.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This function deletes cart
+ */
+ /*function clearCart() {
+	
+	global $dbc;
+    
+    $query = 'TRUNCATE TABLE cart';
+    $statement = $dbc->prepare($query);
+    $statement->execute();
+    $statement->closeCursor();
+	
+ }
+ /*
+  * removes the selected product from the cart
+  */
+/* function removeProduct($remove) {
+	 global $dbc;
+	 
+	$query = 'DELETE FROM cart WHERE product_product_id = :remove';
+    $statement = $dbc->prepare($query);
+	$statement->bindValue(':remove', $remove);
+    $statement->execute();
+    $statement->closeCursor();
+ }
+ 
+ ?>
