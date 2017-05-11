b496456523056328ee08601813a596e912ef66b7
Nathan Mackenzie <ntmk@telus.net>
2017-05-11 15:33:47 -0700

    still working on remove

--- src/cart.php
+++ src/cart.php
@@ -4,22 +4,14 @@
     require_once 'model/db_connect.php';
     require_once 'model/db_functions.php';
 
-	$idp = $_POST['product_id'];
-		$quantity = $_POST['quantity'];
 	
+	$addToCart = addProduct($_POST['product_id'], $_POST['quantity']);
 	
-	
-	
-	if (isset($idp)) {
-			$addToCart = addProduct($idp,$quantity);	
-	}else{
-		$cpid = $_POST['product_id'];
-		$removeItem = removeProduct($cpid);
-	}
+	$removeItem = removeProduct($cpid);
 	$cart = getCart();
 	$total = getTotal();
 	
-	
+	echo "this is the remove value" . $_POST['product_id'] . "<br>";
 	 
 	?>
 
@@ -28,7 +20,6 @@
     <table border=2 rules=rows>
         <thead>
             <tr>
-                
 				<th scope="col">name</th>
                 <th scope="col">price</th>
 				<th scope="col">quantity</th>
@@ -39,19 +30,21 @@
         </thead>
         <tbody>
             <!-- use foreach loop to fetch contents of each row -->	   	
-             <?php foreach ($cart as $item) { ?>
-			 <form action="cart.php" method="post">
 			 
+			<?php foreach ($cart as $item) { ?>
+			<form action="cart.php" method="post">
+			<?php $cpid = $item['product_id']; ?>
+			<input type="hidden" value="<?php echo $cpid; ?>">
 			  <tr>
-				
                 <td><?php echo $item['product_name']; ?></td>
 				<td><?php echo $item['product_price']; ?></td>
 				<td><?php echo $item['quantity']; ?></td>
 				
-				<td><button type="submit" name="remove" id="remove" ><input type="hidden" name="cart_product_id" value="<?php echo $item['product_id']; ?>">Remove</button></td>
-			 </tr>
+				<td><button  type="submit" name="remove">Remove</button></td>
+			</tr>
 			 </form>
 			 <?php } ?>
+			  
 		<tr>
 		<?php foreach ($total as $amount){ ?>
 		<td>subtotal is: <?php echo $amount['total']; ?></td>
