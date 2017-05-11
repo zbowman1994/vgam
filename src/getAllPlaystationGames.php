92daac4e292162cccd6fd62964dbe2ab0a407986
ICS User <ICS User>
2017-05-11 12:50:10 -0700

    made links and added some punctuaion etc.

--- src/getAllPlaystationGames.php
+++ src/getAllPlaystationGames.php
@@ -8,13 +8,13 @@
 	$products = getAllPlaystationGames();
 	?>
 	
-	<button type="button"><a href = "cart.php">Link to cart</a></button>
-	<button type="button"><a href = "getAllProducts.php">all products</a></button>
-<button type="button"><a href = "getAllWiiGames.php">wii games</a></button>
+<button type="button"><a href = "cart.php">Back to cart</a></button>
+<button type="button"><a href = "getAllProducts.php">All products</a></button>
+<button type="button"><a href = "getAllWiiGames.php">Wii games</a></button>
 <button type="button"><a href = "getAllMiscItems.php">Misc Items</a></button>
 <button type="button"><a href = "index.php">Main</a></button>
-<button type="button"><a href = "">contact us</a></button>
-<button type="button"><a href = "">login</a></button>
+<button type="button"><a href = "contact.php">Contact us</a></button>
+<button type="button"><a href = "login.php">Login</a></button>
 	
     <table class="table table-bordered table-striped" id="names">
         <thead>
