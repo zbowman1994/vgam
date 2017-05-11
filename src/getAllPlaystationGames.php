677ff253edd3b5222e1b66a29de0e76c78f557ab
ICS User <ICS User>
2017-05-11 12:58:29 -0700

    Made more links

--- src/getAllPlaystationGames.php
+++ src/getAllPlaystationGames.php
@@ -44,10 +44,10 @@
             <?php } ?>
         </tbody>
     </table>
-	<button type="button"><a href = "cart.php">Link to cart</a></button>
-	<button type="button"><a href = "getAllProducts.php">all products</a></button>
-<button type="button"><a href = "getAllWiiGames.php">wii games</a></button>
+<button type="button"><a href = "cart.php">Back to cart</a></button>
+<button type="button"><a href = "getAllProducts.php">All products</a></button>
+<button type="button"><a href = "getAllWiiGames.php">Wii games</a></button>
 <button type="button"><a href = "getAllMiscItems.php">Misc Items</a></button>
 <button type="button"><a href = "index.php">Main</a></button>
-<button type="button"><a href = "">contact us</a></button>
-<button type="button"><a href = "">login</a></button>
+<button type="button"><a href = "contact.php">Contact us</a></button>
+<button type="button"><a href = "login.php">Login</a></button>
