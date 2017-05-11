16a9e637ecf063c9f7e734bbdcd64713ea6db962
Nathan Mackenzie <ntmk@telus.net>
2017-05-11 09:45:07 -0700

    checkpoint added remove from cart script

--- src/cart.php
+++ src/cart.php
@@ -3,15 +3,16 @@
     
     require_once 'model/db_connect.php';
     require_once 'model/db_functions.php';
+	require_once 'model/deleteFromCart.php';
 	
 	$idp = $_POST['product_id'];
 	$quantity = $_POST['quantity'];
-	$remove = $_GET['priduct_id'];
+	//$remove = $_get['product_id'];
 	
 	$addToCart = addProduct($idp,$quantity);
 	$cart = getCart();
 	$total = getTotal();
-	$removeItem = removeProduct($remove);
+	//$removeItem = removeProduct($remove);
 	
 	 
 	?>
@@ -21,7 +22,7 @@
     <table border=2 rules=rows>
         <thead>
             <tr>
-                <th scope="col">product id</th>
+                
 				<th scope="col">name</th>
                 <th scope="col">price</th>
 				<th scope="col">quantity</th>
@@ -31,22 +32,23 @@
             </tr>
         </thead>
         <tbody>
-            <!-- use foreach loop to fetch contents of each row -->
-	<form action="cart.php" method="get">	   	
+            <!-- use foreach loop to fetch contents of each row -->	   	
              <?php foreach ($cart as $item) { ?>
+			 <form action="deleteFromCart.php" method="get">
+			<input type="hidden" name="product_id" value="<?php echo $product['product_id']; ?>">
 			  <tr>
-				<td><?php echo $item['product_id']; ?></td>
+				
                 <td><?php echo $item['product_name']; ?></td>
 				<td><?php echo $item['product_price']; ?></td>
 				<td><?php echo $item['quantity']; ?></td>
-				<td><input type="checkbox" name="product_id"  ></td>
-				<td><button type="submit" value="<?php $item['product_id'] ?>">Remove</button></td>
+				
+				<td><button type="submit" id="remove" >Remove</button></td>
 			 </tr>
+			 </form>
 			 <?php } ?>
-        </form>
 		<tr>
 		<?php foreach ($total as $amount){ ?>
-		<td>total is: <?php echo $amount['total']; ?></td>
+		<td>subtotal is: <?php echo $amount['total']; ?></td>
 		<?php }?>
 		</tr>
 		</tbody>
