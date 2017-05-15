fa039a26057f400109228bd8b69d85fcf0ff238b
jasmine <jasminekivari@gmail.com>
2017-05-15 15:31:18 -0700

    changes

---
+++ src/vendor/config.php
@@ -0,0 +1,10 @@
+<?php
+require_once('vendor/autoload.php');
+
+$stripe = array(
+  "secret_key"      => "sk_test_BQokikJOvBiI2HlWgH4olfQ2",
+  "publishable_key" => "pk_test_6pRNASCoBOKtIshFeQd4XMUh"
+);
+
+\Stripe\Stripe::setApiKey($stripe['secret_key']);
+?>
