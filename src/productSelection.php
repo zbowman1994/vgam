27ce9e0772b0379bcface85ea384ba6369ee5988
ICS User <ICS User>
2017-05-17 15:23:32 -0700

    Centered products

--- src/productSelection.php
+++ src/productSelection.php
@@ -2,16 +2,14 @@
 <?php require 'view/header.html'; ?>
 <link rel="stylesheet" type ="text/css" href="css/style.css" > 
 
-<button type="button" id="contactus"><a href = "contact.php">Contact Us</a></button>
+<button type="button" id="contactus"><a href = "contact.php">Contact Us</a></button><br>
 
 
 <div style="overflow-x:auto">
-    <table>
+    <table align="center">
       <tr>
         <td>
          <button type="button" id="wii"><a href = "getAllWiiGames.php"><img src="css/images/images.jpg" width="350px" height="300px"/><br>Wii Games</a></button>
-        </td>           
-        <td>
          <button type="button" id="playstationgames"><a href = "getAllPlaystationGames.php"><img src="css/images/ps4.jpg" width="350px" height="300px"/><br>Playstation Games</a></button>
          <button type="button" id="miscitems"><a href = "getAllMiscItems.php"><img src="css/images/download.jpg" width="350px" height="300px"/><br>Misc Items</a></button>
         </td>
