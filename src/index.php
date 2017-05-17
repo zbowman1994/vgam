00554e6a507de07dba55938bc71491127f577e76
Nathan Mackenzie <ntmk@telus.net>
2017-05-17 15:24:53 -0700

    working on email response

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['count'])){
 
 <?php require 'view/header.html'; ?>
 <link rel="stylesheet" type ="text/css" href="css/style.css" >
-
+<?php echo $_SESSION['count'];?>
 <form action="productSelection.php" method="post">
 <button type="button" name="enter" id="products"><a href = "productSelection.php">Experience greatness!</a></button>
 </form>
