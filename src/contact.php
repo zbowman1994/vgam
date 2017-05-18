4ad44455185f750f3ef44191b9514a060f760fdd
ICS User <ICS User>
2017-05-18 14:55:27 -0700

    changed up the contact form and footer

--- src/contact.php
+++ src/contact.php
@@ -3,24 +3,25 @@
 <button type="button" id="products"><a href = "productSelection.php">Product Selection</a></button>
 
 
-<br><h3>Contact Form</h3>
-<div class="container">
-    <form action="contactSubmit.php">
+<br>
+    <form action="contactSubmit.php" id="contact">
+	<h2>Contact Form</h2>
         <label for="fname"></label>First Name<br>
-        <input type="text" id="fname" name="firstname" placeholder="Your first name.."><br>
+        <input type="text" id="fname" name="firstname" placeholder="Your first name.."><br><br>
         <label for="lname"></label>Last Name<br>
-        <input type="text" id="lname" name="lastname" placeholder="Your last name.."><br>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name.."><br><br>
         <label for="country">Country<br></label>
         <select id="country" name="country">
             <option value="canada">Canada</option>
             <option value="australia">Australia</option>     
             <option value="usa">USA</option>
-        </select><br>
+			<option value="other">Other</option>
+        </select><br><br>
         <label for="subject">Subject<br></label>
-        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea><br>
-        <input type="submit" value="Submit"><a href = "contactSubmit.php"></a>
+        <textarea id="subject" name="subject" placeholder="Write something.." rows="17" cols="35"></textarea><br>
+        <input type="submit" value="Submit" id="submitContact"><a href = "contactSubmit.php"></a>
     </form>
-</div>
+
 
 
 <?php require 'view/footer.php'; ?>
