6e92446033d6fb8812720cc0b9099913d3f533d4
jasmine <jasminekivari@gmail.com>
2017-05-18 13:35:01 -0700

    css

--- src/productSelection.php
+++ src/productSelection.php
@@ -10,8 +10,8 @@
       <tr>
         <td>
          <button type="button" id="wii"><a href = "getAllWiiGames.php"><img src="css/images/images.jpg" width="350px" height="300px"/><br>Wii Games</a></button>
-         <button type="button" id="playstationgames"><a href = "getAllPlaystationGames.php"><img src="css/images/ps4.jpg" width="350px" height="300px"/><br>Playstation Games</a></button>
-         <button type="button" id="miscitems"><a href = "getAllMiscItems.php"><img src="css/images/download.jpg" width="350px" height="300px"/><br>Misc Items</a></button>
+         <button type="button" id="playstationGames"><a href = "getAllPlaystationGames.php"><img src="css/images/ps4.jpg" width="350px" height="300px"/><br>Playstation Games</a></button>
+         <button type="button" id="miscItems"><a href = "getAllMiscItems.php"><img src="css/images/download.jpg" width="350px" height="300px"/><br>Misc Items</a></button>
         </td>
        </tr>
     </table>
