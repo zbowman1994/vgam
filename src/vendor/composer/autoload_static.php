59ff6526a40faa8063255b21e20ff8f65e7df6bf
Jason Cumiskey <cumiskey@camosun.bc.ca>
2017-05-19 01:54:34 -0700

    updt vendor/, composer files to include PHPMailer. Change require in charge.php. Should fix issue with deployment

--- src/vendor/composer/autoload_static.php
+++ src/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit9a561037b614d7d65ab43c08fa5465df
+class ComposerStaticInit40b8d35b89b5d6345704a1aedccf9cd6
 {
     public static $prefixLengthsPsr4 = array (
         'S' => 
@@ -20,11 +20,23 @@ class ComposerStaticInit9a561037b614d7d65ab43c08fa5465df
         ),
     );
 
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit9a561037b614d7d65ab43c08fa5465df::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit9a561037b614d7d65ab43c08fa5465df::$prefixDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit40b8d35b89b5d6345704a1aedccf9cd6::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit40b8d35b89b5d6345704a1aedccf9cd6::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit40b8d35b89b5d6345704a1aedccf9cd6::$classMap;
 
         }, null, ClassLoader::class);
     }
