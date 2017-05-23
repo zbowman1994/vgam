fbf00c8f49ab1bb8da1ad7d719cbc283fbde954a
Nathan Mackenzie <ntmk@telus.net>
2017-05-23 14:57:45 -0700

    fixed login form

--- src/contact.php
+++ src/contact.php
@@ -12,9 +12,6 @@
         <input type="text" id="lname" name="lastname" placeholder="Your last name.." required><br><br>
 		<label for="email"></label>Your Email<br>
         <input type="email" id="email" name="email" placeholder="Your email.." required><br><br>
-        <input type="text" id="fname" name="firstname" placeholder="Your first name.." required ><br><br>
-        <label for="lname"></label>Last Name<br>
-        <input type="text" id="lname" name="lastname" placeholder="Your last name.."required ><br><br>
         <label for="country">Country<br></label>
         <select id="country" name="country">
             <option value="canada">Canada</option>
